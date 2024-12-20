f4647fb81319971d3a1b7c92bd84a6c06545971c
bimal <bimaltamang1234@gmail.com>
2024-12-20 23:13:01 +0545

    Add documentation for Singleton design pattern

--- src/main/java/com/learning/designpatterns/creational/Singleton/Main.java
+++ src/main/java/com/learning/designpatterns/creational/Singleton/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main (String[] args){
         SingletonExample singletonExample = SingletonExample.getInstance();
         singletonExample.displayMessage();
+
+        SingletonExample newInstance = SingletonExample.getInstance();
+        if(singletonExample.hashCode() == newInstance.hashCode()){
+            System.out.println(" Same Object found");
+        }
     }
 }
--- src/main/java/com/learning/designpatterns/creational/Singleton/SingletonExample.java
+++ src/main/java/com/learning/designpatterns/creational/Singleton/SingletonExample.java
@@ -1,8 +1,15 @@
 package com.learning.designpatterns.creational.Singleton;
 
+/***
+ *  This SingletonExample class illustrate the example of Singleton design pattern. This class is the singleton
+ *  class. The object will create only once during the class initialization.
+ *
+ */
+
 public class SingletonExample {
 
     static SingletonExample instance = new SingletonExample();
+    // Private constructor refrains to initialization of object by outside of class.
     private SingletonExample(){}
 
     public static SingletonExample getInstance(){
