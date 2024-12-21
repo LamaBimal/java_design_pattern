01f7b64d031fbd374c6e75f2c9820f99bba8bfad
bimal <bimaltamang1234@gmail.com>
2024-12-21 22:36:42 +0545

    Builder Design Pattern Demo Example

--- src/main/java/com/learning/designpatterns/creational/builderpattern/BuilderPatternDemo.java
+++ src/main/java/com/learning/designpatterns/creational/builderpattern/BuilderPatternDemo.java
@@ -1,4 +1,12 @@
-package com.learning.designpatterns.creational.builder;
+package com.learning.designpatterns.creational.builderpattern;
 
 public class BuilderPatternDemo {
+    public static void main(String[] args) {
+        Post post = new Post.Builder()
+                .withTitle("Java Builder Pattern")
+                .withText("Demo Example For Java Builder Pattern")
+                .withCategory("Creational Design Pattern")
+                .build();
+        post.display();
+    }
 }
--- src/main/java/com/learning/designpatterns/creational/builderpattern/Post.java
+++ src/main/java/com/learning/designpatterns/creational/builderpattern/Post.java
@@ -1,4 +1,44 @@
 package com.learning.designpatterns.creational.builderpattern;
 
 public class Post {
+    private final String title;
+    private final String text;
+    private final String category;
+
+    Post(Builder builder){
+        this.title = builder.title;
+        this.text = builder.text;
+        this.category = builder.category;
+    }
+
+    public static class Builder{
+        private String title;
+        private String text;
+        private String category;
+
+        public Builder withTitle(String title){
+            this.title = title;
+            return this;
+        }
+
+        public Builder withText(String text){
+            this.text = text;
+            return this;
+        }
+
+        public Builder withCategory(String category){
+            this.category = category;
+            return this;
+        }
+
+        public Post build(){
+            return new Post(this);
+        }
+    }
+
+    public void display(){
+        System.out.println(" Title : "+this.title);
+        System.out.println(" Text : "+this.text);
+        System.out.println(" Category : "+this.category);
+    }
 }
